a9175f6050c4fde777042c5d02af81036f3f91bc
akucoba231 <aku.coba231@gmail.com>
2026-07-06 01:57:46 +0700

    Add files via upload

---
+++ dataAkun.js
@@ -0,0 +1,31 @@
+// Data Akun Atlet
+const dataAtlet = [
+    {
+        id_atlet: "atlet-1720200000001",
+        nama_atlet: "Milka Karunia Larosa",
+        ttl: "Bekasi, 13 Juli 2008",
+        posisi_pemain: ["Tunggal Putri", "Ganda Putri", "Ganda Campuran"],
+        email: "milkalarosa34@gmail.com",
+        foto: "https://drive.google.com/open?id=1GenCtiUVfUkMWffs_CXYXsUgiu2Yb2f0",
+        club: "PESTI Kabupaten Bekasi",
+        tinggi_badan: "160 cm",
+        lama_latihan: "7 bulan",
+        level_atlet: "Beginner",
+        kelemahan_utama: "Forehand",
+        kata_sandi: "atlet"
+    },
+    {
+        id_atlet: "atlet-1720200000002",
+        nama_atlet: "DENDI IRWANSAH",
+        ttl: "Bekasi, 29 mei, 2008",
+        posisi_pemain: ["Tunggal Putra", "Ganda Putra"],
+        email: "dendyirwansah77@gmail.com",
+        foto: "https://drive.google.com/open?id=10rMParzLhVlA-YZbMZlUuUtlrhUzKejV",
+        club: "Pesti Kabupaten Bekasi",
+        tinggi_badan: "170 cm",
+        lama_latihan: "6 bulan",
+        level_atlet: "Intermediate",
+        kelemahan_utama: "Service",
+        kata_sandi: "atlet"
+    }
+];
