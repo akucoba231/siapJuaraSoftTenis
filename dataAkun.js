c83320fb8921921ceb07b14a62dda5e24d17ce12
akucoba231 <aku.coba231@gmail.com>
2026-08-15 08:25:07 +0700

    Update dataAkun.js

--- dataAkun.js
+++ dataAkun.js
@@ -1,9 +1,12 @@
 // Data Akun Atlet
-const dataAtlet = [
+let dataAtlet = [
     {
         id_atlet: "atlet-1720200000001",
         nama_atlet: "Milka Karunia Larosa",
         ttl: "Bekasi, 13 Juli 2008",
+        usia: "18 Tahun",
+        sekolah: "SMA Negeri 1 Cikarang Utara",
+        alamat: "Jl. Ki Hajar Dewantara No. 91, Cikarang Utara, Bekasi",
         posisi_pemain: ["Tunggal Putri", "Ganda Putri", "Ganda Campuran"],
         email: "milkalarosa34@gmail.com",
         foto: "https://drive.google.com/open?id=1GenCtiUVfUkMWffs_CXYXsUgiu2Yb2f0",
@@ -18,6 +21,9 @@ const dataAtlet = [
         id_atlet: "atlet-1720200000002",
         nama_atlet: "DENDI IRWANSAH",
         ttl: "Bekasi, 29 mei, 2008",
+        usia: "18 Tahun",
+        sekolah: "SMA Negeri 1 Cikarang Pusat",
+        alamat: "Perumahan Deltamas, Cikarang Pusat, Bekasi",
         posisi_pemain: ["Tunggal Putra", "Ganda Putra"],
         email: "dendyirwansah77@gmail.com",
         foto: "https://drive.google.com/open?id=10rMParzLhVlA-YZbMZlUuUtlrhUzKejV",
@@ -27,5 +33,16 @@ const dataAtlet = [
         level_atlet: "Intermediate",
         kelemahan_utama: "Service",
         kata_sandi: "atlet"
+    }
+];
+
+// Data Akun Pelatih
+let dataPelatih = [
+    {
+        id_pelatih: "pelatih-001",
+        nama_pelatih: "Dhani Rohyat, S.Pd",
+        email: "dhanipelatih@gmail.com",
+        kata_sandi: "pelatih",
+        foto: ""
     }
 ];
